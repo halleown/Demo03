42110917985e84cead0616f0e3fe0d9021319474
halleown <2045973656@qq.com>
2026-01-22 14:49:14 +0800

    备份修改密度前

--- app/src/main/java/com/example/demo03/MainActivity.kt
+++ app/src/main/java/com/example/demo03/MainActivity.kt
@@ -4,10 +4,11 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import com.example.demo03.graph.GraphActivity
-import com.example.demo03.scan.QRCodeScanActivity
 import com.example.demo03.spinner.CustomEditorText
 import com.example.demo03.spinner.CustomSpinnerView
 import com.example.demo03.spinner.ViewStatusConfig
--- app/src/main/java/com/example/demo03/scan/QRCodeScanActivity.kt
+++ app/src/main/java/com/example/demo03/scan/QRCodeScanActivity.kt
@@ -1,10 +1,14 @@
 package com.example.demo03.scan
 
 import android.content.Intent
+import android.content.res.Configuration
+import android.content.res.Resources
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.camera.view.PreviewView
 import com.example.demo03.R
 import com.google.zxing.BarcodeFormat
@@ -20,6 +24,7 @@ import com.king.zxing.analyze.MultiFormatAnalyzer
 class QRCodeScanActivity : BarcodeCameraScanActivity() {
 
     private lateinit var myView: ViewfinderView
+    private lateinit var tvScanQR: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +34,7 @@ class QRCodeScanActivity : BarcodeCameraScanActivity() {
         val previewView = findViewById<PreviewView>(R.id.previewView)
         val loading = findViewById<ProgressBar>(R.id.pbCameraLoading)
 
-        findViewById<View>(R.id.btnBack).setOnClickListener {
+        findViewById<View>(R.id.iv_exit).setOnClickListener {
 //            it.isEnabled = false            // 简单防抖，避免多次点击
 //            onBackPressedDispatcher.onBackPressed()
              finish()
@@ -48,7 +53,27 @@ class QRCodeScanActivity : BarcodeCameraScanActivity() {
                 myView.visibility = View.VISIBLE
             }
         }
+
+        tvScanQR = findViewById<TextView>(R.id.tv_scan_qr)
     }
+
+    // override fun getResources(): Resources {
+    //     val res = super.getResources()
+    //     val config = Configuration()
+    //     config.setToDefaults() // 恢复默认配置
+    //     val appMetrics = applicationContext.resources.displayMetrics
+    //     res.updateConfiguration(config, appMetrics)
+    //     return res
+    // }
+
+    // override fun onResume() {
+    //     super.onResume()
+    //     val normalPx = applicationContext.resources.getDimension(R.dimen._24sp)
+    //     tvScanQR.setTextSize(TypedValue.COMPLEX_UNIT_PX, normalPx)
+    //     Log.d("TextSizeLog1", "从全局Context获取的原始px: $normalPx")
+    //     Log.d("TextSizeLog1", "DataManager 24px 对应此页面计算出的 px: $normalPx")
+    //     Log.d("TextSizeLog1", "修复后 qrscan textSize px: ${tvScanQR.textSize}")
+    // }
 
     override fun initCameraScan(cameraScan: CameraScan<Result?>) {
         super.initCameraScan(cameraScan)
@@ -57,13 +82,13 @@ class QRCodeScanActivity : BarcodeCameraScanActivity() {
     }
 
     override fun createAnalyzer(): Analyzer<Result?>? {
-        //初始化解码配置
+        // 初始化解码配置
         val decodeConfig = DecodeConfig()
-        decodeConfig.setHints(DecodeFormatManager.ALL_HINTS) //如果只有识别二维码的需求，这样设置效率会更高，不设置默认为DecodeFormatManager.DEFAULT_HINTS
-            .setFullAreaScan(false) //设置是否全区域识别，默认false
-            .setAreaRectRatio(0.35f) //设置识别区域比例，默认0.8，设置的比例最终会在预览区域裁剪基于此比例的一个矩形进行扫码识别
-            .setAreaRectVerticalOffset(0) //设置识别区域垂直方向偏移量，默认为0，为0表示居中，可以为负数
-            .setAreaRectHorizontalOffset(0) //设置识别区域水平方向偏移量，默认为0，为0表示居中，可以为负数
+        decodeConfig.setHints(DecodeFormatManager.ALL_HINTS) // 如果只有识别二维码的需求，这样设置效率会更高，不设置默认为DecodeFormatManager.DEFAULT_HINTS
+            .setFullAreaScan(false) // 设置是否全区域识别，默认false
+            .setAreaRectRatio(0.35f) // 设置识别区域比例，默认0.8，设置的比例最终会在预览区域裁剪基于此比例的一个矩形进行扫码识别
+            .setAreaRectVerticalOffset(0) // 设置识别区域垂直方向偏移量，默认为0，为0表示居中，可以为负数
+            .setAreaRectHorizontalOffset(0) // 设置识别区域水平方向偏移量，默认为0，为0表示居中，可以为负数
         // BarcodeCameraScanActivity默认使用的MultiFormatAnalyzer，这里也可以改为使用QRCodeAnalyzer
         return MultiFormatAnalyzer(decodeConfig)
     }
